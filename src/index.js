17445fd9d1e1bcc981603a809c2d43c30293aa1b
Shubhanshi <vermashashi3421@gmail.com>
2025-03-08 19:15:07 +0530

    [Shubhanshi]Add. Add functionality to count contacts using reduce

--- src/index.js
+++ src/index.js
@@ -5,18 +5,21 @@ const addressBook = new AddressBook();
 
 try {
     const contact1 = new Contact(
-        "Shubhanshi", "Verma", "Bhopal", "India", "In", "3245", "1234567890", "helloworld@gmail.com"
+        "Shubhanshi", "Verma", "Bhopal", "India", "Madhya Pradesh", "462001", "9876543210", "helloworld@gmail.com"
     );
+    
     console.log(addressBook.addContact(contact1)); 
 
     const contact2 = new Contact(
-        "Ramu", "Patel", "Bhopal", "India","In","1000000","9198563558","chandalala@gmail.com"
-    )
+        "Ramu", "Patel", "Bhopal", "India","Madhya Pradesh","462002","9564873210","chandalala@gmail.com"
+    );
+    
+    
     console.log(addressBook.addContact(contact2)); 
     console.log("All Contacts Before editing", addressBook.getAllContacts());
-    console.log(addressBook.editContact("Rakesh", { address: "Pipariya", city: "New York", phone: "9234567890" }));
+    console.log(addressBook.editContact("Manish", { address: "Raura", city: "London", phone: "2365985628" }));
     console.log("All Contacts After editing", addressBook.getAllContacts());
-    console.log(addressBook.deleteContact("Rakesh"));
+    console.log(addressBook.deleteContact("Arpita"));
     console.log("All Contacts After Deleting:", addressBook.getAllContacts());
 } catch (error) {
     console.error("Error:", error.message);
@@ -24,7 +27,7 @@ try {
 
 try {
     const invalidContact = new Contact(
-        "Maan", "pat", "abc", "Ind", "I", "100A01", "12345", "invalid-email"
+        "Maan", "Patel", "indore", "India", "8978098", "100A01", "12345", "invalid-email"
     );
     console.log(addressBook.addContact(invalidContact));
 } catch (error) {
