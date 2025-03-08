72dc7a829ff0e01032473f2cc2ebc066297f8f5c
Shubhanshi <vermashashi3421@gmail.com>
2025-03-08 19:18:07 +0530

    [Shubhanshi]Add. Add check to prevent duplicate contacts by name

--- src/index.js
+++ src/index.js
@@ -5,29 +5,33 @@ const addressBook = new AddressBook();
 
 try {
     const contact1 = new Contact(
-        "Shubhanshi", "Verma", "Bhopal", "India", "Madhya Pradesh", "462001", "9876543210", "helloworld@gmail.com"
+        "Shubahnshi", "Verma", "Bhopal", "India", "Madhya Pradesh", "462001", "9876543210", "helloworld@gmail.com"
     );
-    
     console.log(addressBook.addContact(contact1)); 
 
     const contact2 = new Contact(
-        "Ramu", "Patel", "Bhopal", "India","Madhya Pradesh","462002","9564873210","chandalala@gmail.com"
+        "Ramu", "Patel", "Bhopal", "India", "Madhya Pradesh", "462002", "9564873210", "chandalala@gmail.com"
     );
-    
-    
     console.log(addressBook.addContact(contact2)); 
+    
     console.log("All Contacts Before editing", addressBook.getAllContacts());
-    console.log(addressBook.editContact("Manish", { address: "Raura", city: "London", phone: "2365985628" }));
+    console.log(addressBook.editContact("Manish", { address: "Raura", city: "London", phone: "9234567890" }));
     console.log("All Contacts After editing", addressBook.getAllContacts());
-    console.log(addressBook.deleteContact("Arpita"));
+    console.log(addressBook.deleteContact("Ramu"));
     console.log("All Contacts After Deleting:", addressBook.getAllContacts());
+    console.log("Total Contacts:", addressBook.countContacts()); 
+
+    const duplicateContact = new Contact(
+        "Shubhanshi", "Verma", "Bhopal", "India", "Madhya Pradesh", "462001", "9876543270", "helloiworld@gmail.com"
+    );
+    console.log(addressBook.addContact(duplicateContact)); 
 } catch (error) {
     console.error("Error:", error.message);
 }
 
 try {
     const invalidContact = new Contact(
-        "Maan", "Patel", "indore", "India", "8978098", "100A01", "12345", "invalid-email"
+        "Ma", "Pa", "Ind", "India", "MP", "100001", "12345", "invalid-email"
     );
     console.log(addressBook.addContact(invalidContact));
 } catch (error) {
