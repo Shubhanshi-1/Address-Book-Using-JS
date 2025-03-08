7bdf4154b843377fd8df60037707a30c4e069be9
Shubhanshi <vermashashi3421@gmail.com>
2025-03-08 19:02:39 +0530

    [Shubhanshi]Add . Added validation for contact details

--- src/index.js
+++ src/index.js
@@ -3,9 +3,21 @@ const Contact = require('./models/Contact');
 
 const addressBook = new AddressBook();
 
-const contact1 = new Contact(
-    "Manish", "Patel", "Bhopal", "India", "In", "3245", "1234567890", "helloworld@gmail.com"
-);
+try {
+    const contact1 = new Contact(
+        "Shubhanshi", "Verma", "Bhopal", "India", "In", "3245", "1234567890", "helloworld@gmail.com"
+    );
+    console.log(addressBook.addContact(contact1));
+    console.log(addressBook.getAllContacts());
+} catch (error) {
+    console.error("Error:", error.message);
+}
 
-console.log(addressBook.addContact(contact1));
-console.log(addressBook.getAllContacts());
+try {
+    const invalidContact = new Contact(
+        "Shubhanshi", "Verma", "abctt", "In", "I", "100A01", "12345", "invalid-email"
+    );
+    console.log(addressBook.addContact(invalidContact));
+} catch (error) {
+    console.error("Error:", error.message);
+}
